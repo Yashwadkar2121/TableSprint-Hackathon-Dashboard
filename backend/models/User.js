49673d5d605a0334ebdb341b2bc869f9eee5f21e
Yash Wadkar <yashwadkar079@gmail.com>
2025-01-06 06:48:16 +0530

    user auth api done

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
-    email: { type: DataTypes.STRING, allowNull: false, unique: true },
-    password: { type: DataTypes.STRING, allowNull: false },
-  });
+  const User = sequelize.define(
+    "User",
+    {
+      email: { type: DataTypes.STRING, allowNull: false, unique: true },
+      password: { type: DataTypes.STRING, allowNull: false },
+    },
+    {
+      timestamps: false, // Disable createdAt and updatedAt
+    }
+  );
   return User;
 };
